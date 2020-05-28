94f366c8e76b6b7068c74a79ce3b080989d62a09
anthonyfinix <anthonyfinix@gmail.com>
2020-05-28 18:24:15 +0530

    welcome loading screen with progressbar

--- src/utils/welcomeLoadingScreen.jsx
+++ src/utils/welcomeLoadingScreen.jsx
@@ -4,9 +4,12 @@ import Typography from '@material-ui/core/Typography';
 import LinearProgress from '@material-ui/core/LinearProgress';
 export default (props)=>{
     return (
-        <Box display="flex" style={{width: '100%',height: '100vh'}}>
-            <Typography>GSTINVOICE</Typography>
-            <LinearProgress />
+        <Box display="flex" flexDirection="column" alignItems="center" justifyContent="center" style={{width: '100%',height: '100vh'}}>
+            <Typography variant="button" gutterBottom >GSTINVOICE</Typography>
+            <div style={{width: 200}}>
+                <LinearProgress/>
+
+            </div>
         </Box>
     )
 }
